00ee337af3bed276e6ce39c0f29c3990d0e24773
kris0504 <kikot0@abv.bg>
2025-07-11 17:21:46 +0300

    new

--- TheCutHub/TheCutHub/Areas/Admin/Controllers/AppointmentsController.cs
+++ TheCutHub/TheCutHub/Areas/Admin/Controllers/AppointmentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TheCutHub.Data;
 
 namespace TheCutHub.Areas.Admin.Controllers
